246849e5b8435044ed7a2f52774e30a740815fa9
milanpaul02 <144575130+milanpaul02@users.noreply.github.com>
2026-08-03 12:59:22 +0200

    Refactor bayesian.py for clarity and performance

--- bayesian.py
+++ bayesian.py
@@ -1,10 +1,12 @@
+#Made with AI assistance
+
 import numpy as np
 import scipy.stats as scs
 import matplotlib.pyplot as plt
 
-## Defining the model that we will use to generate the data
+##Defining the model that we will use to generate the data
 
-#The model is a gaussian vector (X, Y) with mean (0,0) and covariance matrix [[1, rho],[rho, 1]]
+#The model is a gaussian vector (X,Y) with mean (0,0) and covariance matrix [[1,rho],[rho,1]]
 
 def generate_data(rho, n_samples):
     mean = [0, 0]
@@ -14,16 +16,16 @@ def generate_data(rho, n_samples):
 
 ##Estimating the parameters of the model using Bayesian inference for different choices of prior distributions
 
-#Generate a dataset of 500 samples with rho=0.3
-data = generate_data(rho=0.3, n_samples=50)
+#Generate a dataset of 20 samples with rho=0.3
+data = generate_data(rho=0.3, n_samples=20)
 
 #Prior distributions for rho
 
-prior1 = scs.uniform(-1, 2)  # Uniform prior between -1 and 1
+prior1 = scs.uniform(loc=-1, scale=2)  # Uniform prior between -1 and 1
 prior2 = scs.beta(a=2, b=2, loc=-1, scale=2)  # Beta prior transformed to [-1, 1] with a=2, b=2
 prior3 = scs.beta(a=2, b=5, loc=-1, scale=2)  # Beta prior transformed to [-1, 1] with a=2, b=5
 
-#a function to compute the posterior distribution of rho given the data and a prior distribution
+#we define a function to approximately compute the posterior distribution of rho given the data and a prior distribution
 
 def posterior(rho, data, prior):
     n_sample = data.shape[0]
@@ -32,7 +34,6 @@ def posterior(rho, data, prior):
     for i in range(1000):
         #Generate a random variable from the prior distribution
         rho_sample = prior.rvs()
-        #Compute the product of the likelihood and the prior for the sampled value of rho
         likelihood = np.prod(scs.multivariate_normal.pdf(data, mean=[0, 0], cov=[[1, rho_sample], [rho_sample, 1]], allow_singular=True))
         S += likelihood
     I = S / 1000
@@ -40,14 +41,14 @@ def posterior(rho, data, prior):
 
 #Plotting the posterior distributions for different choices of prior distributions and for data samples of size 1, 5, 10 and 20
 
-x = np.linspace(-1, 1, 100)
-n_samples_list = [1, 5, 10, 20]     
+x = np.linspace(-1, 1, 500)
+n_samples_list = [1, 5, 10, 20]
 posterior1 = np.vectorize(lambda rho: posterior(rho, data[:n_samples_list[0]], prior1))(x)
 posterior2 = np.vectorize(lambda rho: posterior(rho, data[:n_samples_list[0]], prior2))(x)
 posterior3 = np.vectorize(lambda rho: posterior(rho, data[:n_samples_list[0]], prior3))(x)
 
 #Plotting the posterior distributions for sample size 1 along with the true value of rho
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(14, 10))
 plt.subplot(2, 2, 1)
 plt.plot(x, posterior1, label='Uniform Prior', color='blue')
 plt.plot(x, posterior2, label='Beta Prior (a=2, b=2)', color='orange')
